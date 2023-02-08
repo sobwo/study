b5f467e0f87f1ecde1c689896f1e85c2085b463a
sobwo <44027786+sobwo@users.noreply.github.com>
2023-02-08 11:45:41 +0900

    Add files via upload

---
+++ StarWrite7.java
@@ -0,0 +1,28 @@
+package javascript1230;
+
+import java.io.*;
+
+public class StarWrite7{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<num;i++) {
+			for(int j=i;j<num-1;j++)
+				bw.write(" ");
+			for(int k=0;k<i*2+1;k++)
+				bw.write("*");
+			bw.write("\n");
+		}
+		for(int i=num;i<num*2-1;i++) {
+			for(int j=num;j<=i;j++)
+				bw.write(" ");
+			for(int k=(num*2-1)*2-1-i;k>i;k--)
+				bw.write("*");
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
