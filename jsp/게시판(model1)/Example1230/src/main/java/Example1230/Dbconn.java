1329a7c06d93b210f503982f5178a77faa6e7cf6
sobwo <so_bw74@naver.com>
2023-03-13 11:41:26 +0900

    23.03.13 jsp학습내용(model1)

---
+++ jsp/게시판(model1)/Example1230/src/main/java/Example1230/Dbconn.java
@@ -0,0 +1,22 @@
+package Example1230;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Dbconn {
+	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private String user = "system";
+	private String password = "1234";
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // 동적로딩 - 메모리에 올림
+			conn = DriverManager.getConnection(url,user,password); // db와 접속시킴, 연결 객체 생성
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return conn;
+	}
+}
