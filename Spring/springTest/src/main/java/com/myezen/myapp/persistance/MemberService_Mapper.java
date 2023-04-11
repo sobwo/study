10c907fb87cc9b86cc766973b838047cc0f58949
sobwo <so_bw74@naver.com>
2023-04-11 17:20:43 +0900

    스프링 db연동(mybatis) 완료

---
+++ Spring/springTest/src/main/java/com/myezen/myapp/persistance/MemberService_Mapper.java
@@ -0,0 +1,16 @@
+package com.myezen.myapp.persistance;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public interface MemberService_Mapper {
+	
+	//마이바티스의 사용할 메소드를 정의한다
+	
+	//public MemberVo memberLogin(String id, String pwd);
+		
+
+	
+}
